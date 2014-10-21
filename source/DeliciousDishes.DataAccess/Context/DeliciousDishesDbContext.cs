ab27bb0b37d8aa1362b991b820d0745ee7f0b057
bruno.hunziker@zuehlke.com <bruno.hunziker@zuehlke.com>
2014-10-21 17:47:35 +0200

    Azure stuff added

--- source/DeliciousDishes.DataAccess/Context/DeliciousDishesDbContext.cs
+++ source/DeliciousDishes.DataAccess/Context/DeliciousDishesDbContext.cs
@@ -6,6 +6,11 @@ namespace DeliciousDishes.DataAccess.Context
 
     public class DeliciousDishesDbContext : DbContext
     {
+        public DeliciousDishesDbContext()
+            : base("DeliciousDishes") 
+        {
+                   
+        }
         public DbSet<DailyOffer> DailyOffers { get; set; }
         public DbSet<Menu> Menus { get; set; }
         public DbSet<MenuOrder> MenuOrders { get; set; }
